6c48ef8a6195ae8ea73048467cec856c07b5d3c6
ThunderASH <ash21aug@gmail.com>
2026-04-27 14:56:24 +0530

    fix: force path for streamlit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import sys, os
 
 from sim.simulator import run_simulations
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../sim")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 st.set_page_config(layout="wide")
 
 # ---------- GLOBAL STYLE ----------
