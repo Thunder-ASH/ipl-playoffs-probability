b9b53e0af45a79fcec18d60b0cf9c533b0a80b12
ThunderASH <ash21aug@gmail.com>
2026-04-29 16:37:06 +0530

    fix: correct import order

--- app/app.py
+++ app/app.py
@@ -83,7 +83,7 @@ div[data-testid="column"] {
 # ---------- TITLE ----------
 st.title("PLAYOFF PROBABILITY")
 st.markdown(
-    "<div class='small' style='margin-bottom:6px;'>As of Apr 28 (RCB v DC)</div>",
+    "<div class='small' style='margin-bottom:6px;'>As of Apr 28 (RR vs PBKS)</div>",
     unsafe_allow_html=True
 )
 
